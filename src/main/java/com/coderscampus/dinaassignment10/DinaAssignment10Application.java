2ca543721feb1abe08f5af0c40fbfa8fb63af82d
dolphin-13 <55006453+dolphin-13@users.noreply.github.com>
2022-01-10 20:26:45 -0800

    Initial commit

---
+++ src/main/java/com/coderscampus/dinaassignment10/DinaAssignment10Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.dinaassignment10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinaAssignment10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinaAssignment10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderscampus/dinaassignment10/DinaAssignment10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.dinaassignment10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinaAssignment10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
